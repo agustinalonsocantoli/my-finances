253383de3bf6b4d0900ac8ab6329468dc9bf56a1
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2023-04-04 15:23:41 +0200

    Navbar completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 // React
 import { useMemo } from "react"
+// Components
+import { Navbar } from "./components/navbar/Navbar"
 // MUI
-import { CssBaseline, ThemeProvider } from "@mui/material"
+import { Box, CssBaseline, ThemeProvider } from "@mui/material"
 import { createTheme } from "@mui/material/styles"
 // Theme
 import { themeSettings } from "./themes/theme"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
     <div className='app'>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+
+            <Routes>
+              <Route path="/" element={<div>Dashboard Page</div>} />
+              <Route path="/predictions" element={<div>Predictions Page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </Router>
     </div>
   )
 }
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,49 @@
+// React
+import { useState } from "react";
+import { Link } from "react-router-dom";
+// MUI
+import { Box, Typography, useTheme } from "@mui/material";
+import { FlexBetween } from "@/themes/components/FlexBetween";
+import WalletIcon from '@mui/icons-material/Wallet';
+
+export const Navbar = () => {
+    const { palette } = useTheme();
+    const [ selected, setSelected ] = useState("Dashboard");
+
+    return(
+        <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+            <FlexBetween gap="0.75rem">
+                <WalletIcon sx={{ fontSize: "28px" }} />
+                <Typography variant="h4" fontSize="16px">
+                    MyFinances
+                </Typography>
+            </FlexBetween>
+
+            <FlexBetween gap="2rem">
+                <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+                    <Link to={"/"} 
+                    onClick={() => setSelected("Dashboard")} 
+                    style={{
+                        color: selected === "Dashboard" ? "inherit" : palette.grey[700],
+                        textDecoration: "inherit"
+                    }}
+                    >
+                        Dashboard
+                    </Link>
+                </Box>
+
+                <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+                    <Link to={"/predictions"} 
+                    onClick={() => setSelected("Predictions")} 
+                    style={{
+                        color: selected === "Predictions" ? "inherit" : palette.grey[700],
+                        textDecoration: "inherit"
+                    }}
+                    >
+                        Predictions
+                    </Link>
+                </Box>
+            </FlexBetween>
+        </FlexBetween>
+    );
+}
